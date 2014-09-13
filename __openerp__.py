19e31e78f168293c781b7e6c54d6355b84c3ea7f
Jaap Jansma <jaap@edeveloper.nl>
2014-09-13 22:30:00 +0200

    initial commit

---
+++ __openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Copyright (c) 2013 Camptocamp SA (http://www.camptocamp.com)
+#   @author Romain Deheele
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{'name': 'Edeveloper',
+ 'version': '1.0.0',
+ 'category': 'Accounting & Finance',
+ 'description': """Allows to print invoices in Edeveloper style""",
+ 'author': 'Edeveloper (Jaap Jansma)',
+ 'website': 'http://www.edeveloper.nl',
+ 'depends': ['base', 'account'],
+ 'data': ['report/account_print_invoice.rml'],
+ 'installable': True,
+ 'active': False,
+ }
